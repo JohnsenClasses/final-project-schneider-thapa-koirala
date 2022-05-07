790ced1b6cfe23c9c27f28532ad1f36256e9e33a
Shekhar Thapa <st15377@uga.edu>
2022-05-07 11:56:44 -0400

    Arduino program for data acquisition

---
+++ Arduino_Program/Sensor_Data_Acquisition_Arduino_Code/Sensor_Data_Acquisition_Arduino_Code.ino
@@ -0,0 +1,37 @@
+//Program code for Arduino Uno to record sensor data and send via serial monitor
+//Sensor data are randomly generated for demonstration
+
+//Temperature and Humidity Variable Initialization
+long loc1_temp; 
+long loc2_temp;
+long loc1_hum;
+long loc2_hum;
+
+int Time = 5000;  //time interval data records
+
+void setup() {
+  Serial.begin(9600);
+  
+}
+void loop() {
+
+//Random sensor data generation, in real world analog signal from sensor can be read from analogRead() function
+loc1_temp=random(80,85);;
+loc2_temp=random(85,90);
+loc2_hum=random(55,60);
+loc1_hum=random(40,45);
+
+//serial monitor outputing in the format: latitude1 Longitude1 Temperature1 Humidity1 Latitude2 Longitude2 Temperature2 Humidity2
+//example: 3.948 -83.377 80 50 31.479 -83.521 85 60
+Serial.print("33.948 -83.377 ");  //Latitude and Longitude of a Location 1
+Serial.print(loc1_temp);          // Temperature1 at Location 1
+Serial.print(" ");
+Serial.print(int(loc1_hum));      // Humidity1 at Location 1
+Serial.print(" ");
+Serial.print("31.479 -83.521 ");   // Latitude and Longitude of a Location 2
+Serial.print(loc2_temp);          //Temperature1 at Location 2
+Serial.print(" ");
+Serial.println(int(loc2_hum));    //Humidity at Location 2
+  
+delay(Time); //Time interval between data records
+}
